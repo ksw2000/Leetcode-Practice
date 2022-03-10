358399e0a17dcbfd00e787d3ca7b2ce2d6854b9a
liao2000 <liao2000@livemail.tw>
2022-03-10 23:38:41 +0800

    Create rotate-list.go

---
+++ rotate-list/rotate-list.go
@@ -0,0 +1,34 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return head
+	}
+	end := head
+	for i := 0; i < k; i++ {
+		if end.Next == nil {
+			end = head
+			k = k % (i + 1)
+			for j := 0; j < k; j++ {
+				end = end.Next
+			}
+			break
+		} else {
+			end = end.Next
+		}
+	}
+	start := head
+	for end.Next != nil {
+		end = end.Next
+		start = start.Next
+	}
+	end.Next = head
+	ret := start.Next
+	start.Next = nil
+	return ret
+}
